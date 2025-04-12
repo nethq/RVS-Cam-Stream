9f9bb4dd12e97da8c180def472f93de353dcb32b
azimuth-m <ifandakov@gmail.com>
2025-04-12 12:57:16 +0000

    Added CMakeLists.txt
    - Sucessfully building INSIDE DOCKER aarch64
      skeleton executable
    
    Signed-off-by: azimuth-m <ifandakov@gmail.com>

--- scripts/setup_qcs6490.sh
+++ scripts/setup_qcs6490.sh
@@ -11,27 +11,40 @@ function rvs-config-testapp-qcs6490() {
     }
 
     # Create needed build and out directories
-    [ -d "${RVS_ROOT_DIR}/out" ]                                            && {
-        mkdir "${RVS_ROOT_DIR}/out"
-    }
-
-    [ -d "${RVS_ROOT_DIR}/build" ]                                          && {
+    [ ! -d "${RVS_ROOT_DIR}/build" ]                                       && {
         mkdir "${RVS_ROOT_DIR}/build"
     }
 
     (
-        cd "${PROJECT_BINARY_DIR}/build"
-        cmake "${PROJECT_BINARY_DIR}/src"
+        cd "${RVS_ROOT_DIR}/build"
+        cmake "${RVS_ROOT_DIR}"
+    )
+}
+
+function rvs-build-testapp-qcs6490() {
+
+    # Sanity
+    [ -z "${RVS_ROOT_DIR}" ] && {
+        echo -e "Environment varaible \"RVS_ROOT_DIR\" not set properly.\n`
+                `Source the setenv.sh in the root of the project, while in the `
+                `same directory as it is."
+        return -1
+    }
+
+    (
+        cd "${RVS_ROOT_DIR}/build"                                          && \
+                cmake --build "${RVS_ROOT_DIR}/build"                       && \
+                cmake --install "${RVS_ROOT_DIR}/build"
     )
 }
 
 echo -e "\n####################################################################`
         `############"
 echo "rvs-config-testapp-qcs6490()"
-echo "    Used to configure the cmake build environment."
+echo -e "    Used to configure the cmake build environment.\n"
 
-echo -e "\n####################################################################`
-        `############"
 echo "rvs-build-testapp-qcs6490()"
 echo "    Used to build the usb-cam-to-web testapp."
 
+echo -e "####################################################################`
+        `############"
